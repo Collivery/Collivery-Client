a9afc973e66a8cfee76c91ba0ea7dd51b59ff324
Bryce Large <bryce@jiro.co.za>
2014-05-20 12:33:15 +0200

    Add clear all cache
    Added so we can clear all cache in cache directory

--- src/Mds/Cache.php
+++ src/Mds/Cache.php
@@ -62,4 +62,36 @@ class Cache {
 			return false;
 		}
 	}
+
+	public function clear() {
+		return $this->delete_files(__DIR__ . $this->cache_dir, true);
+	}
+
+	function delete_files($path, $del_dir = FALSE, $level = 0)
+	{
+		$path = rtrim($path, DIRECTORY_SEPARATOR);
+
+		if (!$current_dir = @opendir($path)) {
+			return FALSE;
+		}
+
+		while (FALSE !== ($filename = @readdir($current_dir))) {
+			if ($filename != "." and $filename != "..") {
+				if (is_dir($path . DIRECTORY_SEPARATOR . $filename)) {
+					if (substr($filename, 0, 1) != '.') {
+						delete_files($path . DIRECTORY_SEPARATOR . $filename, $del_dir, $level + 1);
+					}
+				} else {
+					unlink($path . DIRECTORY_SEPARATOR . $filename);
+				}
+			}
+		}
+		@closedir($current_dir);
+
+		if ($del_dir == TRUE AND $level > 0) {
+			return @rmdir($path);
+		}
+
+		return TRUE;
+	}
 }
